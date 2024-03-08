91dcf807b5a8fb925e8e84749582e74afdbcc9df
Vyshnav R <vyshnavr856@gmail.com>
2024-03-08 15:30:57 +0530

    minor changes

--- backend/cloud.py
+++ backend/cloud.py
@@ -2,6 +2,7 @@ import boto3
 
 access_key = ""
 secret_access_key = ""
+bucket_name = ""
 
 def save_to_cloud():
 	s3 = boto3.resource(
@@ -12,10 +13,9 @@ def save_to_cloud():
 	)
 
 	s3 = boto3.client("s3",
-					aws_access_key_id="AKIAWT4JAZLZ2WSVTRDB",
-					aws_secret_access_key="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")
+					aws_access_key_id=access_key,
+					aws_secret_access_key=secret_access_key)
 
-	bucket_name = "ranensk-bucket"
 	image = "final_image.png"
 	output = "output.txt"
 
